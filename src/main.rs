300962b63e81517bd22b113f17c6badd8fe31d04
NutiNaguti <derzhavin.danila@icloud.com>
2022-11-25 17:18:04 +0400

    added log subscriber

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,61 @@
+use hex_literal::hex;
 use std::env;
-use web3::types::BlockId;
-use web3::types::BlockNumber;
-use web3::types::U64;
+use std::str::FromStr;
+use web3::api::{EthSubscribe, SubscriptionStream};
+use web3::futures::{future, StreamExt};
+use web3::signing;
+use web3::types::{BlockId, BlockNumber, FilterBuilder, H160, H2048, U64};
 
 #[tokio::main]
 async fn main() -> web3::Result<()> {
     let args: Vec<String> = env::args().collect();
-    let block_number = &args[1];
-    let block_number = BlockId::Number(BlockNumber::Number(
-        U64::from_dec_str(&block_number).unwrap(),
-    ));
-    println!("block number: {:?}", block_number);
 
-    let transport = web3::transports::Http::new(
-        "https://eth-goerli.g.alchemy.com/v2/fq5FsW3IggL1giodxIhWRqW-er0MpDbi",
-    )?;
+    // let block_number = &args[1];
+    // let block_number = BlockId::Number(BlockNumber::Number(
+    //     U64::from_dec_str(&block_number).unwrap(),
+    // ));
+
+    let contract_address = &args[1];
+    let normalized_address: H160 = H160::from_str(contract_address).unwrap();
+
+    let transport = web3::transports::WebSocket::new(
+        "wss://eth-goerli.g.alchemy.com/v2/fq5FsW3IggL1giodxIhWRqW-er0MpDbi",
+    )
+    .await?;
     let web3 = web3::Web3::new(transport);
 
-    let block = web3.eth().block(block_number).await?;
-    match block {
-        Some(data) => println!("logs bloom: {:?}", data.logs_bloom),
-        _ => {}
-    }
+    // let block = web3.eth().block(block_number).await?;
+    // let logs_bloom = block.unwrap().logs_bloom.unwrap();
+    // println!("logs filter: {}", logs_bloom);
+
+    // TODO
+    // let encoded_address = signing::keccak256(contract_address.as_bytes());
+    // let encoded_event_signature = signing::keccak256(&signing::keccak256(
+    //     "Locked(uint256,address,uint256)".as_bytes(),
+    // ));
+    // let encoded_logged_data = signing::keccak256(&signing::keccak256(logged_data.as_bytes()));
+    // Filter for Hello event in our contract
+
+    let filter = FilterBuilder::default()
+        .address(vec![normalized_address])
+        .topics(
+            Some(vec![hex!(
+                "55016b6cc60c3d15b7b1ebd0ab766c07b3082c98a5b4d1d7ff012a97652a4b1d"
+            )
+            .into()]),
+            None,
+            None,
+            None,
+        )
+        .build();
+
+    let sub = web3.eth_subscribe().subscribe_logs(filter).await?;
+
+    sub.for_each(|log| {
+        println!("got log: {:?}", log);
+        future::ready(())
+    })
+    .await;
 
     Ok(())
 }

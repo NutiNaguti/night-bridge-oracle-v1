3e6ecfbadebe1b30080ca1f2357e4c83db8ba324
NutiNaguti <derzhavin.danila@icloud.com>
2022-11-26 16:44:58 +0400

    researching ways to get and validate logs filter

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,27 @@
 use hex_literal::hex;
 use std::env;
 use std::str::FromStr;
+use web3::ethabi::ethereum_types::BloomInput;
 use web3::futures::{future, StreamExt};
-use web3::types::{FilterBuilder, H160};
+use web3::transports::WebSocket;
+use web3::types::{BlockId, BlockNumber, FilterBuilder, H160, H2048, U64};
+use web3::{signing, Web3};
 
 #[tokio::main]
 async fn main() -> web3::Result<()> {
     let args: Vec<String> = env::args().collect();
 
-    // let block_number = &args[1];
-    // let block_number = BlockId::Number(BlockNumber::Number(
-    //     U64::from_dec_str(&block_number).unwrap(),
-    // ));
+    let block_number = &args[1];
+    let block_number = BlockId::Number(BlockNumber::Number(
+        U64::from_dec_str(&block_number).unwrap(),
+    ));
 
-    let contract_address = &args[1];
-    let normalized_address: H160 = H160::from_str(contract_address).unwrap();
+    let contract_address = &args[2];
+    let normalized_address = H160::from_str(contract_address).unwrap();
+
+    let event_data_1 = 8023385;
+    let event_data_2 = H160::from_str("0x8cab5e96e1ab09e8678a8ffc75b5d818e73d4707").unwrap();
+    let event_data_3 = 12;
 
     let transport = web3::transports::WebSocket::new(
         "wss://eth-goerli.g.alchemy.com/v2/fq5FsW3IggL1giodxIhWRqW-er0MpDbi",
@@ -22,18 +29,37 @@ async fn main() -> web3::Result<()> {
     .await?;
     let web3 = web3::Web3::new(transport);
 
-    // let block = web3.eth().block(block_number).await?;
-    // let logs_bloom = block.unwrap().logs_bloom.unwrap();
-    // println!("logs filter: {}", logs_bloom);
+    let block = web3.eth().block(block_number).await?;
+    let logs_bloom = block.unwrap().logs_bloom.unwrap();
 
-    // TODO
-    // let encoded_address = signing::keccak256(contract_address.as_bytes());
-    // let encoded_event_signature = signing::keccak256(&signing::keccak256(
-    //     "Locked(uint256,address,uint256)".as_bytes(),
-    // ));
-    // let encoded_logged_data = signing::keccak256(&signing::keccak256(logged_data.as_bytes()));
-    // Filter for Hello event in our contract
+    println!("logs filter: {:?}", logs_bloom);
 
+    let contains = logs_bloom.contains_input(BloomInput::Hash(&signing::keccak256(
+        &signing::keccak256("Locked3(uint256,address,uint256)".as_bytes()),
+    )));
+
+    println!("Event signature: {}", contains);
+
+    let contains = logs_bloom.contains_input(BloomInput::Hash(&signing::keccak256(
+        normalized_address.as_bytes(),
+    )));
+
+    println!("Contract address: {}", contains);
+
+    // If we join data step by step then we are getting too big hex number
+    // I think this is wrong algorithm
+    // Need more research
+    let contains = logs_bloom.contains_input(BloomInput::Raw(&signing::keccak256(
+        &signing::keccak256(H2048::from_str("").unwrap().as_bytes()),
+    )));
+
+    println!("Event data: {}", contains);
+
+    Ok(())
+}
+
+// Not used but may be useful
+async fn eth_subscribe(web3: Web3<WebSocket>, normalized_address: H160) -> web3::Result<()> {
     let filter = FilterBuilder::default()
         .address(vec![normalized_address])
         .topics(
